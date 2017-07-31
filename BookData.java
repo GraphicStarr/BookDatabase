ba29dd6d0b449f5f880e8a6aed7e04c64c387dcb
GraphicStarr <jerilynmensah@gmail.com>
2017-07-31 10:27:09 -0400

    Add files via upload
    Update on code

--- BookData.java
+++ BookData.java
@@ -1,20 +1,78 @@
 /**
  * 
  */
+
 package book;
 
 /**
- * @author student
- *
- */
-public class BookData {
+ * @author GraphicStarr
+ /**
+  * This application will contain a method to return a book object.
+  * This application will also create a list of books from said database and look
+  * up books, according to its SKU, title and author and display all contents, including
+  * the description and the price.
+  */
+import java.io.*;
 
-	/**
-	 * @param args
-	 */
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class BookData {
+	
+	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
-	}
-
+	}	
+		Object String = null;
+		Scanner scanner = new Scanner(System.in);
+		int skuNum = 0;
+		String bookTitle = null;
+		String descript = null;
+		double price = 0.0;
+		String author = null
+		
+		
+		
+		
+		// Prompt the user to enter the SKU of book
+				
+		do{	
+			System.out.println("Does the user know the SKU of the book? Y/N");
+			skuNum = scanner.nextInteger();
+			
+			if(answer.equalsIgnoreCase("y")){
+			System.out.println("Enter the SKU of the book");
+	   
+	   // Prompt the user to enter the title of book
+			
+			}else{
+				System.out.println("Does the user know the title of the book? Y/N");
+			}
+			if(answer.equalsIgnoreCase("y")){
+				System.out.println("Enter the title of book");
+				
+	  // Prompt the user to enter the title or the author of book
+			}else{
+				System.out.println("Does the user know the title or author of book Y/N");
+			
+			}if (answer.equalsIgnoreCase("y")){
+			else if{ 
+				System.out.println("Enter the author of book"); 
+			}else if{
+				System.out.prinln("Enter the title of book");
+			}
+			else{
+				System.out.println("Does the user want to look up another book Y/N");
+			
+	  // Prompt the user to look up another book
+			}if (answer.equalsIgnoreCase("y")){
+				System.otu.println()
+			}
+				
+				
+				
+			}
+		}while(answer.equalsIgnoreCase("y"));
 }
